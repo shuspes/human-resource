0694203737ea8c364fe4fc49c8656e7e7c03c29f
shuspes <shuspes@gmail.com>
2017-06-23 01:20:45 +0300

    Add new properties to peoples. Update peoples data.

--- data/peoples.js
+++ data/peoples.js
@@ -1,71 +1,107 @@
 export const peoples = [
-    {
-        Id: "1",
-        FirstName: "Pol",
-        LastName: "White",
-        FullName: "Pol White",
-        Gender: "male",
-        Age: "34",
-        Email: "pol.white@fake.com",
-        Phone: "375-33-4561289"
-    },
-    {
-        Id: "2",
-        FirstName: "Jane",
-        LastName: "Mustang",
-        FullName: "Jane Mustang",
-        Gender: "female",
-        Age: "25",
-        Email: "jane.mustang@fake.com",
-        Phone: "375-29-9876734"
-    },
-    {
-        Id: "3",
-        FirstName: "Dean",
-        LastName: "Jonhson",
-        FullName: "Dean Jonhson",
-        Gender: "male",
-        Age: "56",
-        Email: "dean.jonhson@fake.com",
-        Phone: "375-44-6548965"
-    }
+  {
+    Id: "1",
+    FirstName: "Pol",
+    LastName: "White",
+    FullName: "Pol White",
+    Gender: "male",
+    Age: "34",
+    Email: "pol.white@fake.com",
+    Phone: "375-33-4561289",
+    CompanyName: "White Circle",
+    CurrentPosition: "manager",
+    DesiredPosition: "manager"
+  },
+  {
+    Id: "2",
+    FirstName: "Jane",
+    LastName: "Mustang",
+    FullName: "Jane Mustang",
+    Gender: "female",
+    Age: "25",
+    Email: "jane.mustang@fake.com",
+    Phone: "375-29-9876734",
+    CompanyName: "Pepelac",
+    CurrentPosition: "developer",
+    DesiredPosition: "team lead"
+  },
+  {
+    Id: "3",
+    FirstName: "Dean",
+    LastName: "Jonhson",
+    FullName: "Dean Jonhson",
+    Gender: "male",
+    Age: "56",
+    Email: "dean.jonhson@fake.com",
+    Phone: "375-44-6548965",
+    CompanyName: "no-name",
+    CurrentPosition: "designer",
+    DesiredPosition: "top designer"
+  }
 ];
 
 export const peoplesTableColumns = [
-    {
-        Code: "FirstName",
-        Type: "string",
-        Name: "First Name",
-        DisplayOrder: 0
-    },
-    {
-        Code: "LastName",
-        Type: "string",
-        Name: "Last Name",
-        DisplayOrder: 1
-    },
-    {
-        Code: "Gender",
-        Type: "select",
-        Name: "Gender",
-        DisplayOrder: 2
-    },
-    {
-        Code: "Age",
-        Type: "number",
-        Name: "Age",
-        DisplayOrder: 3
-    },
-    {
-        Code: "Email",
-        Type: "string",
-        Name: "Email",
-        DisplayOrder: 4
-    },
-    {
-        Code: "Phone",
-        Type: "string",
-        Name: "Phone",
-        DisplayOrder: 5
-    }
+  {
+    Code: "FirstName",
+    Type: "string",
+    Name: "First Name",
+    DisplayOrder: 0,
+    ShouldDisplayInTable: true
+  },
+  {
+    Code: "LastName",
+    Type: "string",
+    Name: "Last Name",
+    DisplayOrder: 1,
+    ShouldDisplayInTable: true
+  },
+  {
+    Code: "Gender",
+    Type: "select",
+    Name: "Gender",
+    DisplayOrder: 2,
+    ShouldDisplayInTable: true
+  },
+  {
+    Code: "Age",
+    Type: "number",
+    Name: "Age",
+    DisplayOrder: 3,
+    ShouldDisplayInTable: true
+  },
+  {
+    Code: "Email",
+    Type: "string",
+    Name: "Email",
+    DisplayOrder: 4,
+    ShouldDisplayInTable: false
+  },
+  {
+    Code: "Phone",
+    Type: "string",
+    Name: "Phone",
+    DisplayOrder: 5,
+    ShouldDisplayInTable: false
+  },
+  {
+    Code: "CompanyName",
+    Type: "string",
+    Name: "Company",
+    DisplayOrder: 6,
+    ShouldDisplayInTable: false
+  },
+  {
+    Code: "CurrentPosition",
+    Type: "string",
+    Name: "CurrentPosition",
+    DisplayOrder: 7,
+    ShouldDisplayInTable: false
+  },
+  {
+    Code: "DesiredPosition",
+    Type: "string",
+    Name: "DesiredPosition",
+    DisplayOrder: 8,
+    ShouldDisplayInTable: true
+  }
 ];
